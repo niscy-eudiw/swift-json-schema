347d7a1dfd60ff220745f9c8e6cf46fca972e0bf
dtsiflit <dimitri.tsiflitzis@netcompany.com>
2025-06-12 15:51:14 +0300

    [fix] removed functionality, kept schema only

--- Sources/JSONSchemaBuilder/JSONPropertyComponent/Modifier/JSONPropertyComponents.swift
+++
@@ -1,2 +0,0 @@
-/// A namespace for all the JSON property components.
-public enum JSONPropertyComponents {}
--- Sources/JSONSchemaMacro/JSONSchemaMacroPlugin.swift
+++
@@ -1,9 +0,0 @@
-import SwiftCompilerPlugin
-import SwiftSyntaxMacros
-
-@main struct JSONSchemaMacroPlugin: CompilerPlugin {
-  let providingMacros: [Macro.Type] = [
-    SchemableMacro.self, SchemaOptionsMacro.self, NumberOptionsMacro.self, ArrayOptionsMacro.self,
-    ObjectOptionsMacro.self, StringOptionsMacro.self, ExcludeFromSchemaMacro.self,
-  ]
-}
